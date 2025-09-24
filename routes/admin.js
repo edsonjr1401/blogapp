fd5d93f7ef4d3edcf066e75a98b0bdf6d1c18f75
Edson Junior <108370882+edsonjr1401@users.noreply.github.com>
2025-09-24 13:49:48 -0300

    first commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    res.send("Pagina principal do painel ADM")
+})
 
+router.get('/posts', (req, res) =>{
+    res.send("Página de posts")
+})
 
-
+router.get("/categorias", (req, res) => {
+    
+})
 
 
 

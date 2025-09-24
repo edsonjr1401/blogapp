cdd473b244b88fdb388170da2c7adfc66c8fb9d3
Edson Junior <108370882+edsonjr1401@users.noreply.github.com>
2025-09-24 13:37:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Carregando módulo
+     const express = require('express')
+     const handlebars = require('express-handlebars')
+     const bodyParser = require("body-parser")
+     const app = express()
+
+
+// Configurações 
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    // Handlebars 
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars');
+        
+// Rotas 
+
+
+// Outros
+const PORT = 8081
+app.listen(PORT, function() {
+    console.log("Servidor rodando!")
+})

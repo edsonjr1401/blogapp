b7b4f6695ba5ba9b44d2ba1debfda7a7da83da78
Edson Junior <108370882+edsonjr1401@users.noreply.github.com>
2025-10-13 11:38:39 -0300

    att

---
+++ models/Usuario.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/bd'); 
+
+const Usuario = db.define('NomeDoModelo', {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+ {
+    tableName: 'nome_da_tabela'
+});
+
+module.exports = NomeDoModelo;

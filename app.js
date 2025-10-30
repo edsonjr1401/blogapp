ddd4934fec8ab817131447ca130fceb36e9b1b0c
Edson Junior <108370882+edsonjr1401@users.noreply.github.com>
2025-10-30 13:24:57 -0300

    att

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // =======================================================================
 // IMPORTAÇÕES
 // =======================================================================
+const path = require("path");
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
-const path = require("path");
 
 // Rotas
 const admin = require("./routes/admin");
@@ -19,24 +19,20 @@ const main = require("./routes/main");
 // =======================================================================
 const app = express();
 
-// Configuração do Passport
-require("./config/auth")(passport);
-
 // =======================================================================
 // MIDDLEWARES
 // =======================================================================
 
-// Sessão
+// Configuração do Passport
+require("./config/auth")(passport);
+
+// Sessão, Flash e Passport Middlewares
 app.use(session({
-    secret: "cursodenode",
+    secret: process.env.SESSION_SECRET || "cursodenode", // Use environment variable for production
     resave: true,
     saveUninitialized: true
 }));
-
-// Flash Messages
 app.use(flash());
-
-// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -56,11 +52,11 @@ app.use((req, res, next) => {
     next();
 });
 
-// Body Parser
+// Body Parser Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Handlebars
+// Configuração de Views (Handlebars) e Arquivos Estáticos
 const hbs = exphbs.create({
     defaultLayout: 'main',
     helpers: {
@@ -70,8 +66,6 @@ const hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
-
-// Arquivos Estáticos
 app.use(express.static(path.join(__dirname, "public")));
 
 // =======================================================================
